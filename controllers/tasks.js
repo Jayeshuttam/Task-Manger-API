2ce46e0ea6c3961d795d25111bf3eb44bdc3f539
Jayeshuttam <jayeshuttam7844@gmail.com>
2022-01-26 13:17:03 -0500

    Second commit
    changes in task Controller and created new custom Error class

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,7 @@
 const Task = require ('../models/task.js')
 const asyncWrapper = require('../middleware/async')
+const {createCustomError}=require('../errors/custom-error')
+const 
 //Gets all tasks from database 
 const getAllTasks = asyncWrapper(async (req,res)=>{
         const tasks= await Task.find({})
@@ -19,10 +21,8 @@ const getTask = asyncWrapper(async (req,res,next) => {
         const{id:taskID} = req.params
         const task= await Task.findOne({_id:taskID})
         if (!task){
-            const error = new Error('Not found')
-            error.status = 404
-            return next(error)
-            return res.status(404).json({msg:`No task with id :${taskID} `})
+            return next(createCustomError(`No task with id :${taskID} `,404))
+      //      return res.status(404).json({msg:`No task with id :${taskID} `})
         }
         res.status(200).json({task})
     
